aaeeae1eaa60eff36ef3a058b92335fd3e6e4eeb
james10949 <james48118510@gmail.com>
2020-07-02 20:09:43 +0800

    設定Moderators功能

--- cmds/moderations.py
+++ cmds/moderations.py
@@ -11,6 +11,26 @@ class Moderations(commands.Cog):
 	async def on_ready(self):
 		print(f'loaded cog: {__name__}')
 
+	@commands.command()
+	async def kick(self, ctx, member: discord.Member, *, reason = None):
+		pass
+
+	@kick.error
+	async def kick_error(self, ctx, error):
+		if isinstance(error, commands.MissingPermissions):
+			await ctx.send("Kick failed\nReason:Missing Administrator Permissions")
+
+	@commands.command()
+	@commands.has_permissions(administrator = True)
+	async def ban(self, ctx, member: discord.Member, *, reason = None):
+		await member.ban(reason=reason)
+		await ctx.send(f'Ban {member} complete\nReason:{reason}')
+
+	@ban.error
+	async def ban_error(self, ctx, error):
+		if isinstance(error, commands.MissingPermissions):
+			await ctx.send("Ban failed\nReason:Missing Administrator Permissions")
+
 
 def setup(bot):
 	bot.add_cog(Moderations(bot))
