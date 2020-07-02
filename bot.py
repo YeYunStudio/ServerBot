78edeef6d8e29729886355f3ff1a2e25f553a154
james10949 <james48118510@gmail.com>
2020-07-02 17:59:43 +0800

    新贈Help和prefix功能

--- bot.py
+++ bot.py
@@ -8,12 +8,20 @@ with open('config.json', 'r', encoding='utf8') as bot:
 
 bot = commands.Bot(command_prefix=config['prefix'])
 
-
 @bot.event
 async def on_ready():
 	print(f'Logged in as: {bot.user.name}')
 	print(f'With ID: {bot.user.id}')
 	print(f'Prefix: {bot.command_prefix}')
+
+bot.remove_command('help')
+@bot.command()
+async def help(ctx):
+	await ctx.send(config['help'])
+
+@bot.command()
+async def prefix(ctx):
+	await ctx.send(f'在這個Server的Prefix是`{bot.command_prefix}`唷')
 
 keep_alive.keep_alive()
 bot.run(config['token'])
