c6cf9e05e62ddd6a351ec4318fb9863bea64afec
james10949 <james48118510@gmail.com>
2020-07-03 02:29:55 +0800

    更新加入離開訊息

--- cmds/events.py
+++ cmds/events.py
@@ -1,6 +1,10 @@
 import discord
 from discord.ext import commands
+import json
+from discord import Guild
 
+with open ('config.json', 'r', encoding='utf8') as data:
+	config = json.load(data)
 
 class Events(commands.Cog):
 	def __init__(self, bot):
@@ -11,6 +15,15 @@ class Events(commands.Cog):
 	async def on_ready(self):
 		print(f'loaded cog: {__name__}')
 
+	@commands.Cog.listener()
+	async def on_member_join(self, member):
+		channel = self.bot.get_channel(int(config['welcome_channel']))
+		await channel.send(f'{member.mention}'+ '進入了'+ f'{member.guild.name}!')
+
+	@commands.Cog.listener()
+	async def on_member_remove(self, member):
+		channel = self.bot.get_channel(int(config['goodbye_channel']))
+		await channel.send(f'**{member.name}#{member.discriminator}**'+ '離開了'+ f'{member.guild.name}')
 
 def setup(bot):
 	bot.add_cog(Events(bot))
