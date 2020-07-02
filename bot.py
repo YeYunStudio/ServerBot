0c3ef44b5f81de654f4e6c7b60f1e8d16aecfe7f
james10949 <james48118510@gmail.com>
2020-07-02 21:21:34 +0800

    啟用Cog系統

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import json
 import os
 import keep_alive
 
-with open('config.json', 'r', encoding='utf8') as data:
+with open ('config.json', 'r', encoding='utf8') as data:
 	config = json.load(data)
 
 bot = commands.Bot(command_prefix=config['prefix'])
@@ -24,6 +24,66 @@ async def help(ctx):
 @bot.command()
 async def prefix(ctx):
 	await ctx.send(f'在這個Server的Prefix是`{bot.command_prefix}`唷')
+
+@bot.command()
+@commands.is_owner()
+async def enable(ctx, extension):
+	bot.load_extension(f'cmds.{extension}')
+	await ctx.send(f'已起用{extension}')
+	
+@enable.error
+async def enable_error(ctx, error):
+	if isinstance(error, commands.NotOwner):
+		await ctx.send("您無法使用這個指令\n原因:不是機器人的擁有者")
+	raise error
+
+@bot.command()
+@commands.is_owner()
+async def disable(ctx, extension):
+	bot.disable_extension(f'cmds.{extension}')
+	await ctx.send(f'已禁用{extension}')
+
+@disable.error
+async def disable_error(ctx, error):
+	if isinstance(error, commands.NotOwner):
+		await ctx.send('您無法使用這個指令\n原因:不是機器人的擁有者')
+	raise error
+
+@bot.command()
+@commands.is_owner()
+async def reload(ctx, extension):
+	bot.reload_extension(f'cmds.{extension}')
+	await ctx.send(f'已重新載入{extension}')
+
+@reload.error
+async def reload_error(ctx, error):
+	if isinstance(error, commands.NotOwner):
+		await ctx.send('您無法使用這個指令\n原因:不是機器人的擁有者')
+	raise error
+
+@bot.command()
+async def invite(ctx):
+	await ctx.send(config['invite'])
+
+@bot.command()
+@commands.is_owner()
+async def shutdown(parameter_list):
+	pass
+
+@shutdown.error
+async def shutdown_error(ctx, error):
+	if isinstance(error, commands.NotOwner):
+		await ctx.send('您無法使用這個指令\n原因:不是機器人的擁有者')
+
+@bot.command()
+@commands.is_owner()
+async def reboot(parameter_list):
+	pass
+
+@reboot.error
+async def reboot_error(ctx, error):
+	if isinstance(error, commands.NotOwner):
+		await ctx.send('您無法使用這個指令\n原因:不是機器人的擁有者')
 
 for filename in os.listdir('./cmds'):
 	if filename.endswith('.py'):
