1769b6c9bc64bed36989cff23fd5215ca8dc8b40
james10949 <james48118510@gmail.com>
2020-07-07 20:26:00 +0800

    新增clean指令+錯誤訊息中文化

--- cmds/moderations.py
+++ cmds/moderations.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import time
 
 
 class Moderations(commands.Cog):
@@ -13,24 +14,37 @@ class Moderations(commands.Cog):
 
 	@commands.command()
 	async def kick(self, ctx, member: discord.Member, *, reason = None):
-		pass
+		await member.kick(reason=reason)
+		await ctx.send(f'已踢出{member}\n理由：{reason}')
 
 	@kick.error
 	async def kick_error(self, ctx, error):
 		if isinstance(error, commands.MissingPermissions):
-			await ctx.send("Kick failed\nReason:Missing Administrator Permissions")
+			await ctx.send('無法使用kick指令\n理由：缺少相關權限(管理員)')
 
 	@commands.command()
 	@commands.has_permissions(administrator = True)
 	async def ban(self, ctx, member: discord.Member, *, reason = None):
 		await member.ban(reason=reason)
-		await ctx.send(f'Ban {member} complete\nReason:{reason}')
+		await ctx.send(f'已封鎖{member}\n理由：{reason}')
 
 	@ban.error
 	async def ban_error(self, ctx, error):
 		if isinstance(error, commands.MissingPermissions):
-			await ctx.send("Ban failed\nReason:Missing Administrator Permissions")
+			await ctx.send('無法使用ban指令\n理由：缺少相關權限(管理員)')
+	
+	@commands.command()
+	@commands.has_permissions(manage_messages=True)
+	async def clean(self, ctx, num : int):
+		await ctx.channel.purge(limit = num+1)
+		await ctx.send(f'已刪除{num}條訊息')
+		time.sleep(5)
+		await ctx.channel.purge(limit = 1)
 
+	@clean.error
+	async def clean_error(self, ctx, error):
+		if isinstance(error, commands.MissingPermissions):
+			await ctx.send(f"無法使用clean指令\n理由：缺少相關權限(管理訊息)")
 
 def setup(bot):
 	bot.add_cog(Moderations(bot))
